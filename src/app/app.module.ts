44963c41e5b96b7981bc239045d9cd97ad8f2f1d
dmmontero <danny.montero@gmail.com>
2018-10-10 16:17:18 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Camera } from '@ionic-native/camera';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+      SplashScreen, Camera,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  myPhoto: any;
+  constructor(public navCtrl: NavController, private camera: Camera) {}
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
 
-  constructor(public navCtrl: NavController) {
-
+    this.camera.getPicture(options).then(
+      imageData => {
+        this.myPhoto = 'data:image/jpeg;base64,' + imageData;
+        console.log(this.myPhoto);
+      },
+      err => {}
+    );
   }
-
 }
